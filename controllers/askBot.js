3022387cca2c6fed15f398c7499669baa96160ec
deepak427 <62477872+deepak427@users.noreply.github.com>
2023-03-26 11:11:15 +0530

    first commit

--- controllers/askBot.js
+++ controllers/askBot.js
@@ -61,7 +61,7 @@ export const verify = async (req, res) => {
       `
     })
 
-    console.log(a)
+    console.log(transporter, a)
 
     const token = jwt.sign({ email, otp }, process.env.JWT_SECRECT, {
       expiresIn: "1h",
